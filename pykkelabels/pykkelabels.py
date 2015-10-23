917b4c0a977f1b5965dac48dd344eab130019f56
Anders Brandt <anderswb@gmail.com>
2015-10-23 23:42:36 +0200

    Got the first couple of get methods working

--- pykkelabels/pykkelabels.py
+++ pykkelabels/pykkelabels.py
@@ -28,6 +28,8 @@
 
 import urllib.request
 import urllib.error
+import urllib.parse
+import json
 
 class Pykkelabels:
     API_ENDPOINT = 'https://app.pakkelabels.dk/api/public/v2'
@@ -35,23 +37,17 @@ class Pykkelabels:
     def __init__(self, api_user, api_key):
         self._api_user = api_user
         self._api_key = api_key
+        self._token = None
         self.login()
-        urllib.request.urlopen(Pykkelabels.API_ENDPOINT)
 
     def login(self):
-        # Create an OpenerDirector with support for Basic HTTP Authentication...
-        auth_handler = urllib.request.HTTPBasicAuthHandler()
-        auth_handler.add_password(None, Pykkelabels.API_ENDPOINT, self._api_user, self._api_key);
-        
-        opener = urllib.request.build_opener(auth_handler)
-        # ...and install it globally so it can be used with urlopen.
-        urllib.request.install_opener(opener)
+        result = self._make_api_call('users/login', True, {'api_user': self._api_user, 'api_key': self._api_key})
+        self._token = result['token']
 
-#    public function balance(){
-#        $result = $this->_make_api_call('users/balance');
-#        return $result['balance'];
-#    }
-#
+    def balance(self):
+        result = self._make_api_call('users/balance')
+        return result['balance']
+
 #    public function pdf($id){
 #        $result = $this->_make_api_call('shipments/pdf', false, array('id' => $id));
 #        return $result['base64'];
@@ -87,48 +83,49 @@ class Pykkelabels:
 #        return $result;
 #    }
 #
-#    public function freight_rates(){
-#        $result = $this->_make_api_call('shipments/freight_rates');
-#        return $result;
-#    }
+    def freight_rates(self):
+        result = self._make_api_call('shipments/freight_rates')
+        return result
+
 #
 #    public function payment_requests(){
 #        $result = $this->_make_api_call('users/payment_requests');
 #        return $result;
 #    }
 #
-#    public function gls_droppoints($params){
-#        $result = $this->_make_api_call('shipments/gls_droppoints', false, $params);
-#        return $result;
-#    }
+    def gls_droppoints(self, params):
+        result = self._make_api_call('shipments/gls_droppoints', False, params)
+        return result
+
 #
 #    public function pdk_droppoints($params){
 #        $result = $this->_make_api_call('shipments/pdk_droppoints', false, $params);
 #        return $result;
 #    }
 
-    #def getToken(self):
-    #    return this._token
+    def getToken(self):
+        return self._token
 
-#    def _make_api_call(method, doPost = False, params = dict()):
-#        #$ch = curl_init()
-#        urllib.request
-#        params['token'] = this._token;
-#
-#        $query = http_build_query($params);    
-#        if doPost:
-#            curl_setopt($ch, CURLOPT_URL, self::API_ENDPOINT . '/' . $method);
-#            curl_setopt($ch, CURLOPT_POST, 1);
-#            curl_setopt($ch, CURLOPT_POSTFIELDS, $query);
-#        else:
-#            curl_setopt($ch, CURLOPT_URL, self::API_ENDPOINT . '/' . $method . '?' . $query);
-#
+    def _make_api_call(self, method, doPost = False, params = dict()):
+        params['token'] = self._token
+        params = urllib.parse.urlencode(params)
+        
+        if doPost:
+            url = Pykkelabels.API_ENDPOINT + '/' + method
+            f = urllib.request.urlopen(url, params.encode('utf-8'))
+        else:
+            url = Pykkelabels.API_ENDPOINT + '/' + method + '?%s' % params
+            print(url)
+            f = urllib.request.urlopen(url)
+            
 #        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 #
 #        $output = curl_exec ($ch);
 #        $http_code = curl_getinfo( $ch, CURLINFO_HTTP_CODE);
 #        curl_close ($ch);
-#
+        output = f.read().decode('utf-8')
+        outputparsed = json.loads(output)
+        
 #        $output = json_decode($output, true);
 #
 #        if http_code != 200:
@@ -139,8 +136,13 @@ class Pykkelabels:
 #                    die($output['message']);
 #            }
 #
-#        return output
+        return outputparsed
 
 
 if __name__ == '__main__':
     pl = Pykkelabels('871684fe-b098-4d04-a96a-527865f162da', '073e628f-c8b6-4911-87b5-63b6fa451fc6')
+    #print('Token: {}'.format(pl.getToken()))
+    #print('Balance: {}'.format(pl.balance()))
+    print(pl.gls_droppoints({'zipcode': '2300'}))
+    #print(pl.freight_rates())
+    
