24f71ab469c3a0c3c663c1de78a973c74f65c923
Anders Brandt <anderswb@gmail.com>
2015-10-24 00:15:47 +0200

    Ported the rest of the methods to python

--- pykkelabels/pykkelabels.py
+++ pykkelabels/pykkelabels.py
@@ -4,25 +4,25 @@
 #Usage:
 #----------------
 #The first thing required is to login
-#$label = new Pakkelabels('api_user', 'api_key'); 
+#label = new Pakkelabels('api_user', 'api_key'); 
 #
 #This will login and fetch the required token. 
 #The token is then automatically added to any subsequent calls. 
 #
 #To see the generated token you can use:
-#echo $label->getToken();
+#echo label->getToken();
 #
 #Examples:
 #----------------
 #// Get all Post Danmark labels shipped to Denmark
-#$labels = $label->shipments(array('shipping_agent' => 'pdk', 'receiver_country' => 'DK'));
+#labels = label->shipments(array('shipping_agent' => 'pdk', 'receiver_country' => 'DK'));
 #
 #// Display the PDF for a specific label
-#$base64 = $label->pdf(31629);
-#$pdf = base64_decode($base64);
+#base64 = label->pdf(31629);
+#pdf = base64_decode(base64);
 #header('Content-type: application/pdf');
 #header('Content-Disposition: inline; filename="label.pdf"');
-#echo $pdf;
+#echo pdf;
 #*/
 #
 
@@ -48,60 +48,49 @@ class Pykkelabels:
         result = self._make_api_call('users/balance')
         return result['balance']
 
-#    public function pdf($id){
-#        $result = $this->_make_api_call('shipments/pdf', false, array('id' => $id));
-#        return $result['base64'];
-#    }
-#
-#    public function zpl($id){
-#        $result = $this->_make_api_call('shipments/zpl', false, array('id' => $id));
-#        return $result['base64'];
-#    }
-#    
-#    public function shipments($params = array()){
-#        $result = $this->_make_api_call('shipments/shipments', false, $params);
-#        return $result;
-#    }
-#    
-#    public function imported_shipments($params = array()){
-#        $result = $this->_make_api_call('shipments/imported_shipments', false, $params);
-#        return $result;
-#    }
-#
-#    public function create_imported_shipment($params){
-#        $result = $this->_make_api_call('shipments/imported_shipment', true, $params);
-#        return $result;
-#    }
-#    
-#    public function create_shipment($params){
-#        $result = $this->_make_api_call('shipments/shipment', true, $params);
-#        return $result;
-#    }
-#
-#    public function create_shipment_own_customer_number($params){
-#        $result = $this->_make_api_call('shipments/shipment_own_customer_number', true, $params);
-#        return $result;
-#    }
-#
+    def pdf(self, id):
+        result = self._make_api_call('shipments/pdf', False, {'id': id})
+        return result['base64']
+
+    def zpl(self, id):
+        result = self._make_api_call('shipments/zpl', False, {'id': id})
+        return result['base64']
+    
+    def shipments(self, params = dict()):
+        result = self._make_api_call('shipments/shipments', False, params)
+        return result
+    
+    def imported_shipments(self, params = dict()):
+        result = self._make_api_call('shipments/imported_shipments', False, params)
+        return result
+
+    def create_imported_shipment(self, params):
+        result = self._make_api_call('shipments/imported_shipment', True, params)
+        return result
+    
+    def create_shipment(self, params):
+        result = self._make_api_call('shipments/shipment', True, params)
+        return result
+
+    def create_shipment_own_customer_number(self, params):
+        result = self._make_api_call('shipments/shipment_own_customer_number', True, params)
+        return result
+
     def freight_rates(self):
         result = self._make_api_call('shipments/freight_rates')
         return result
 
-#
-#    public function payment_requests(){
-#        $result = $this->_make_api_call('users/payment_requests');
-#        return $result;
-#    }
-#
+    def payment_requests(self):
+        result = self._make_api_call('users/payment_requests')
+        return result
+
     def gls_droppoints(self, params):
         result = self._make_api_call('shipments/gls_droppoints', False, params)
         return result
 
-#
-#    public function pdk_droppoints($params){
-#        $result = $this->_make_api_call('shipments/pdk_droppoints', false, $params);
-#        return $result;
-#    }
+    def pdk_droppoints(params):
+        result = self._make_api_call('shipments/pdk_droppoints', False, params)
+        return result
 
     def getToken(self):
         return self._token
@@ -115,34 +104,33 @@ class Pykkelabels:
             f = urllib.request.urlopen(url, params.encode('utf-8'))
         else:
             url = Pykkelabels.API_ENDPOINT + '/' + method + '?%s' % params
-            print(url)
             f = urllib.request.urlopen(url)
-            
-#        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-#
-#        $output = curl_exec ($ch);
-#        $http_code = curl_getinfo( $ch, CURLINFO_HTTP_CODE);
-#        curl_close ($ch);
+
+#        curl_setopt(ch, CURLOPT_RETURNTRANSFER, true);
+
+#        http_code = curl_getinfo( ch, CURLINFO_HTTP_CODE);
+
         output = f.read().decode('utf-8')
         outputparsed = json.loads(output)
-        
-#        $output = json_decode($output, true);
-#
+
 #        if http_code != 200:
-#            if(is_array($output['message'])){
-#                    print_r($output['message']);
+#            if(is_array(output['message'])):
+#                    print_r(output['message']);
 #                    die();
 #            }else{
-#                    die($output['message']);
+#                    die(output['message']);
 #            }
-#
         return outputparsed
 
 
 if __name__ == '__main__':
     pl = Pykkelabels('871684fe-b098-4d04-a96a-527865f162da', '073e628f-c8b6-4911-87b5-63b6fa451fc6')
-    #print('Token: {}'.format(pl.getToken()))
-    #print('Balance: {}'.format(pl.balance()))
-    print(pl.gls_droppoints({'zipcode': '2300'}))
+    print('Token: {}'.format(pl.getToken()))
+    print('Balance: {}'.format(pl.balance()))
+    print('GLS droppoints:')
+    for droppoint in pl.gls_droppoints({'zipcode': '2300'}):
+        print(' ------------------')
+        for key, value in droppoint.items():
+            print(' ' + key + ': ' + value)
     #print(pl.freight_rates())
     
