1ad1408f741ed4c7120608d53f28703454d9f8c7
Anders Brandt <anderswb@gmail.com>
2015-10-24 18:52:10 +0200

    added more tests

--- test/test_pykkelabels.py
+++ test/test_pykkelabels.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 import unittest
 from pykkelabels.pykkelabels import *
 
@@ -6,6 +8,7 @@ from decimal import *
 
 API_USER = '492ace80-8b40-4369-8ee7-ca4457984ffb'
 API_KEY = 'da04a9e9-1563-4aaf-b443-3012ca6c2772'
+
 
 class goodinput(unittest.TestCase):
 
@@ -17,11 +20,33 @@ class goodinput(unittest.TestCase):
 
     def test_balance(self):
         pl = Pykkelabels(API_USER, API_KEY)
-        self.assertEquals(Decimal(0.0), pl.balance())
+        self.assertEqual(Decimal(0.0), pl.balance())
 
     def test_getToken(self):
         pl = Pykkelabels(API_USER, API_KEY)
-        self.assertEquals(40, len(pl.getToken()))
+        self.assertEqual(40, len(pl.getToken()))
+
+    def test_pdkdroppoints(self):
+        pl = Pykkelabels(API_USER, API_KEY)
+        exp_points = [{'zipcode': '2300', 'city': 'KØBENHAVN S', 'address': 'Brydes Allé 34', 'number': '3830',
+                       'company_name': 'Pakkeboks 3830 Dagli Brugsen'},
+                      {'zipcode': '2300', 'city': 'KØBENHAVN S', 'address': 'Englandsvej 28', 'number': '626',
+                       'company_name': 'Pakkeboks 626 Kvickly'},
+                      {'zipcode': '2300', 'city': 'KØBENHAVN S', 'address': 'Englandsvej 28', 'number': '626',
+                       'company_name': 'Pakkeboks 626 Kvickly Handikapvenlig'}]
+        points = pl.pdk_droppoints({'zipcode': '2300'})
+        self.assertEqual(points, exp_points)
+
+    def test_glsdroppoints(self):
+        pl = Pykkelabels(API_USER, API_KEY)
+        exp_points = [{'number': '95913', 'city': 'København S', 'company_name': 'Dagli´Brugsen Brydes Allé',
+                       'address2': 'Pakkeshop: 95913', 'address': 'Brydes\xa0Allé 34', 'zipcode': '2300'},
+                      {'number': '95422', 'city': 'København S', 'company_name': 'PC Update',
+                       'address2': 'Pakkeshop: 95422', 'address': 'Amagerbrogade 109', 'zipcode': '2300'},
+                      {'number': '95423', 'city': 'København S', 'company_name': 'Centerkiosken',
+                       'address2': 'Pakkeshop: 95423', 'address': 'Reberbanegade 3', 'zipcode':'2300'}]
+        points = pl.gls_droppoints({'zipcode': '2300'})
+        self.assertEqual(points, exp_points)
 
 if __name__ == '__main__':
     unittest.main()
