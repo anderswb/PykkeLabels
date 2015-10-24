02472e5ef7febba74e25d64ff6e92e64576ddbfa
Anders Brandt <anderswb@gmail.com>
2015-10-24 12:48:24 +0200

    Begun porting the docstring to python

--- pykkelabels/pykkelabels.py
+++ pykkelabels/pykkelabels.py
@@ -1,22 +1,26 @@
-#<?php
-#
-#/*
-#Usage:
-#----------------
-#The first thing required is to login
-#label = new Pakkelabels('api_user', 'api_key'); 
-#
-#This will login and fetch the required token. 
-#The token is then automatically added to any subsequent calls. 
-#
-#To see the generated token you can use:
-#echo label->getToken();
-#
-#Examples:
-#----------------
-#// Get all Post Danmark labels shipped to Denmark
-#labels = label->shipments(array('shipping_agent' => 'pdk', 'receiver_country' => 'DK'));
-#
+#!/usr/bin/env python
+
+""" Python implementation of the Pakkelabels.dk php package for interacting with the Pakkelabels.dk web service.
+For documentation on usage and the methods, see the documentation on https://www.pakkelabels.dk/integration/api/
+
+Usage:
+----------------
+The first thing required is to login
+label = new Pykkelabels('api_user', 'api_key')
+
+This will login and fetch the required token.
+The token is then automatically added to any subsequent calls.
+
+To see the generated token you can use:
+print(label.getToken())
+
+Examples:
+----------------
+Get all Post Danmark labels shipped to Denmark
+labels = label.shipments({'shipping_agent': 'pdk', 'receiver_country': 'DK'})
+
+"""
+
 #// Display the PDF for a specific label
 #base64 = label->pdf(31629);
 #pdf = base64_decode(base64);
