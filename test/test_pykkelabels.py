56ac4e9be241caeb069eb1b8455571b085a748f6
Anders Brandt <anderswb@gmail.com>
2015-10-25 17:53:41 +0100

    Test will now read the api user and key from ini file

--- test/test_pykkelabels.py
+++ test/test_pykkelabels.py
@@ -5,31 +5,28 @@ from pykkelabels.pykkelabels import *
 
 import urllib.error
 from decimal import *
-
 import base64
-
-API_USER = '492ace80-8b40-4369-8ee7-ca4457984ffb'
-API_KEY = 'da04a9e9-1563-4aaf-b443-3012ca6c2772'
+import configparser
 
 
-class goodinput(unittest.TestCase):
+class GoodInput(unittest.TestCase):
 
     def test_login(self):
         try:
-            Pykkelabels(API_USER, API_KEY)
+            Pykkelabels(self.api_user, self.api_key)
         except urllib.error.HTTPError:
             self.fail('Did not log in properly')
 
     def test_balance(self):
-        pl = Pykkelabels(API_USER, API_KEY)
+        pl = Pykkelabels(self.api_user, self.api_key)
         self.assertEqual(Decimal(0.0), pl.balance())
 
     def test_getToken(self):
-        pl = Pykkelabels(API_USER, API_KEY)
+        pl = Pykkelabels(self.api_user, self.api_key)
         self.assertEqual(40, len(pl.getToken()))
 
     def test_pdkdroppoints(self):
-        pl = Pykkelabels(API_USER, API_KEY)
+        pl = Pykkelabels(self.api_user, self.api_key)
         exp_points = [{'zipcode': '2300', 'city': 'KØBENHAVN S', 'address': 'Brydes Allé 34', 'number': '3830',
                        'company_name': 'Pakkeboks 3830 Dagli Brugsen'},
                       {'zipcode': '2300', 'city': 'KØBENHAVN S', 'address': 'Englandsvej 28', 'number': '626',
@@ -40,18 +37,18 @@ class goodinput(unittest.TestCase):
         self.assertEqual(points, exp_points)
 
     def test_glsdroppoints(self):
-        pl = Pykkelabels(API_USER, API_KEY)
+        pl = Pykkelabels(self.api_user, self.api_key)
         exp_points = [{'number': '95913', 'city': 'København S', 'company_name': 'Dagli´Brugsen Brydes Allé',
                        'address2': 'Pakkeshop: 95913', 'address': 'Brydes\xa0Allé 34', 'zipcode': '2300'},
                       {'number': '95422', 'city': 'København S', 'company_name': 'PC Update',
                        'address2': 'Pakkeshop: 95422', 'address': 'Amagerbrogade 109', 'zipcode': '2300'},
                       {'number': '95423', 'city': 'København S', 'company_name': 'Centerkiosken',
-                       'address2': 'Pakkeshop: 95423', 'address': 'Reberbanegade 3', 'zipcode':'2300'}]
+                       'address2': 'Pakkeshop: 95423', 'address': 'Reberbanegade 3', 'zipcode': '2300'}]
         points = pl.gls_droppoints({'zipcode': '2300'})
         self.assertEqual(points, exp_points)
 
     def test_freight_rates(self):
-        pl = Pykkelabels(API_USER, API_KEY)
+        pl = Pykkelabels(self.api_user, self.api_key)
         rates = pl.freight_rates()
 
         # Test a couple of things, which should be in the returned dict
@@ -64,7 +61,7 @@ class goodinput(unittest.TestCase):
         self.assertEqual(rates['DK']['pdk']['name'], 'Post Danmark')
 
     def test_payment_requests(self):
-        pl = Pykkelabels(API_USER, API_KEY)
+        pl = Pykkelabels(self.api_user, self.api_key)
         requests = pl.payment_requests()
         self.assertIsInstance(requests, list)
         self.assertEqual(0, len(requests))
@@ -88,7 +85,7 @@ class goodinput(unittest.TestCase):
                   'services': '11,12',
                   'test': 'true'}
 
-        pl = Pykkelabels(API_USER, API_KEY)
+        pl = Pykkelabels(self.api_user, self.api_key)
         result = pl.create_shipment(params)
         self.assertEqual(result['pkg_no'], '00000000000000000000')
         self.assertEqual(result['order_id'], '0000')
@@ -99,6 +96,15 @@ class goodinput(unittest.TestCase):
             referencepdf = f.read()
 
         self.assertEqual(pdfpayload, referencepdf)
+
+    def setUp(self):
+        try:
+            config = configparser.ConfigParser()
+            config.read('testsettings.ini')
+            self.api_user = config.get('login', 'api_user')
+            self.api_key = config.get('login', 'api_key')
+        except:
+            raise Exception('Unable to read the required settings from the ini file.')
 
 
 if __name__ == '__main__':
