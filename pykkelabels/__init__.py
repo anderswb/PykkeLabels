134c652ad74bbc333dc6a49dc336fbd2f5e54d87
Anders Brandt <anderswb@gmail.com>
2015-10-28 20:30:07 +0100

    Adjusted package

--- pykkelabels/__init__.py
+++ pykkelabels/__init__.py
@@ -3,7 +3,7 @@ from .exceptions import *
 
 __author__ = "Anders Brandt"
 __license__ = "MIT"
-__version__ = "0.1.3"
+__version__ = "0.1.4"
 __maintainer__ = "Anders Brandt"
 __email__ = "anderswb at  gmail dot com"
 __status__ = "Development"
--- release_to_pypi.bat
+++ release_to_pypi.bat
@@ -1,6 +1,3 @@
-set HOME=C:\Users\Anders\
 C:\Python35\python.exe setup.py bdist_egg upload --identity="Anders Brandt" --sign --quiet
-C:\Python35\python.exe setup.py bdist_wininst --target-version=3.4 register upload --identity="Anders Brandt" --sign --quiet
-C:\Python35\python.exe setup.py bdist_wininst --target-version=3.5 register upload --identity="Anders Brandt" --sign --quiet
 C:\Python35\python.exe setup.py sdist upload --identity="Anders Brandt" --sign
 pause
