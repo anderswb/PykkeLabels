32fde051d7a335d01a10899cda5c2b60a447b104
Anders Brandt <anderswb@gmail.com>
2015-10-30 19:37:38 +0100

    Removed examples etc, as it is already in the readme file

--- pykkelabels/pykkelabels.py
+++ pykkelabels/pykkelabels.py
@@ -3,26 +3,7 @@
 
 """ Python implementation of the Pakkelabels.dk php package for interacting with the Pakkelabels.dk web service.
 For documentation on usage and the methods, see the documentation on https://www.pakkelabels.dk/integration/api/
-
-Usage:
-----------------
-The first thing required is to login
-label = new Pykkelabels('api_user', 'api_key')
-
-This will login and fetch the required token.
-The token is then automatically added to any subsequent calls.
-
-To see the generated token you can use:
-print(label.getToken())
-
-Examples:
-----------------
-Get all Post Danmark labels shipped to Denmark:
-labels = label.shipments({'shipping_agent': 'pdk', 'receiver_country': 'DK'})
-
-Get the PDF for a specific label:
-base64 = label.pdf(31629)
-pdf = base64.decode('base64')
+and README.md
 """
 
 try:
